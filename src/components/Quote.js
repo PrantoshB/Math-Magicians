4233aef76a6c8edc891218a5643b02a2df69a93d
PrantoshB <pbiswas1007@gmail.com>
2023-01-17 18:09:00 +0530

    Build: Create Quote page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 
-function Quote(){
-    return (
-        <div className="quotes">
-            <p>"Mathematics is the language with which God has written the universe." — Galileo Galilei</p>
-        </div>
-    )
+function Quote() {
+  return (
+    <div className="quotes">
+      <p>
+        &quot;Mathematics is the language with which God has written the
+        universe.&quot; — Galileo Galilei
+      </p>
+    </div>
+  );
 }
 
 export default Quote;
