4e77e19c237fda2b011d14eaca43f12dfad5bde0
PrantoshB <pbiswas1007@gmail.com>
2023-01-17 17:59:38 +0530

    Build: Create Quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Quote(){
+    return (
+        <div className="quotes">
+            <p>"Mathematics is the language with which God has written the universe." — Galileo Galilei</p>
+        </div>
+    )
+}
+
+export default Quote;
