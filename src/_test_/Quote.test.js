c03e8d7e7cdd33adaf90f3d8585137bd8084d212
Harshita Jain <harshitaforever06@gmail.com>
2023-01-19 14:06:08 +0530

    changes

--- src/_test_/Quote.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Quote from '../components/Quote';
-
-it('renders when there is quote', () => {
-  const tree = renderer.create(<Quote />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
