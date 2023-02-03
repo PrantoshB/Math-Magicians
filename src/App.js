be048672fcf39d1952bb9575bd936a54974f1000
PrantoshB <pbiswas1007@gmail.com>
2023-02-03 14:38:47 +0000

    Add mobile version

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
 import Quote from './components/Quote';
+import logo from './logo.png';
 
 const App = () => (
   <>
     <nav className="navigation">
-      <h1>Math Magicians</h1>
+      <div className="img-title">
+        <img src={logo} alt="logo" />
+        <h1>Math Magicians</h1>
+      </div>
       <ul>
         <Link to="/">Home</Link>
         <Link to="/calculator">Calculator</Link>
